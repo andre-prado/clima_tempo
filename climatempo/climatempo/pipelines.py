3bd38f2c954cc6ee0eb9c7799f06ce5feaf2bb14
andre-prado <andr3prad0@gmail.com>
2021-01-25 15:33:52 -0300

    cria conexao com mongodb e atualiza requirements.txt

--- climatempo/climatempo/pipelines.py
+++ climatempo/climatempo/pipelines.py
@@ -5,9 +5,22 @@
 
 
 # useful for handling different item types with a single interface
+import pymongo
+
 from itemadapter import ItemAdapter
+from .connection import password
+
+class MongodbPipeline:
+    collection_name = "climatempo_dados"
 
 
-class ClimatempoPipeline:
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(f"mongodb+srv://admin:{password}@cluster0.tsc5b.mongodb.net/<dbname>?retryWrites=true&w=majority")
+        self.db = self.client["climatempo"]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    
     def process_item(self, item, spider):
-        return item
+        self.db[self.collection_name].insert(item)
